88180643cd23425f3ec1b80620bc2439be838f98
Kamodes <38344216+Kamodes@users.noreply.github.com>
2022-03-11 04:51:30 +0000

    roomTABLEの作成

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `room`;
 CREATE TABLE `user` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -7,3 +8,16 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 );
+
+CREATE TABLE `room` (
+  `room_id` bigint NOT NULL AUTO_INCREMENT,
+  `live_id` int DEFAULT NULL,
+  `joined_user_count` int DEFAULT NULL,
+  `max_user_count` int DEFAULT NULL,
+  PRIMARY KEY (`room_id`)
+)
+
+CREATE TABLE `room_member` (
+  `id` bigint NOT NULL,
+  `live_id` bigint NOT NULL,
+)
